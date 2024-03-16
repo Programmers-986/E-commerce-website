51960393e144c3c3d16c1a2dcc2e478d26328e5c
Nandan Upadhyaya <nandanupadhyaya18@gmail.com>
2024-03-16 20:27:54 +0530

    Optimized Hero Section

--- src/app/Components/Pages/Hero.tsx
+++ src/app/Components/Pages/Hero.tsx
@@ -8,22 +8,24 @@ const Hero = () => {
     return (
         <div className="flex h-screen bg-gradient-to-b from-purple-100 to-green-200 bg-opacity-50">
             <div className="flex-1 flex flex-col justify-start pl-16 leading-7 ml-10 mt-24">
-                <h2 className="text-2xl font-bold pt-10 mb-10">New Releases Only {/*<Image src={hand_icon} alt="" width={50} height={50} />*/}</h2>
-                <div className="text-5xl font-bold mb-">
-                    <p className="ml-1 mb-5">New</p>
-                    <p className="mb-5">Collections</p>
-                    <p className="mb-1">For Everyone</p>
+                <h2 className="text-3xl font-semibold pt-10 mb-10 ml-40">New Releases Only {/*<Image src={hand_icon} alt="" width={50} height={50} />*/}</h2>
+                <div className="text-7xl font-bold ">
+                    <p className="ml-40 mb-5">New</p>
+                    <p className="mb-5 ml-40">Collections</p>
+                    <p className="mb-1 ml-40">For Everyone</p>
                 </div>
                 
-                <div className="flex items-center justify-start mt-10">
+                <div className="flex items-center justify-start mt-10 mb-30 ml-40">
                     <div className="bg-red-500 text-white rounded-full py-2 px-6 text-lg font-semibold flex items-center cursor-pointer">
                         Latest Collection
-                        <Image src={arrow_icon} alt="" width={30} height={30} className="ml-2" />
+                        <Image src={arrow_icon} alt="" width={30} height={60}  />
                     </div>
                 </div>
             </div>
-            <div className="flex items-center justify-center">
-                <Image src={hero_image} alt="" width={400} height={400} />
+            <div className="flex-1 items-center justify-center mr-30 flex flex-col">
+                <div className="flex items-center justify-center ">
+                    <Image src={hero_image} alt="" width={550} height={800} />
+                </div>
             </div>
         </div>
     );
